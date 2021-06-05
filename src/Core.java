ddcd89fff942c3f549666a2598e06c915ba95c94
Prashanth-96 <71920861+Prashanth-96@users.noreply.github.com>
2021-06-05 22:32:47 +0530

    UC1 and UC2

--- src/Core.java
+++ src/Core.java
@@ -6,8 +6,17 @@ public class Core{
 		y1=	(int) (Math.random()*100);
 		x2=(int) (Math.random()*100);
 		y2=(int) (Math.random()*100);
+		
+		float  x1b= (int) (Math.random()*100);
+		float  y1b=(int) (Math.random()*100);
+		float  x2b=(int) (Math.random()*100);
+		float y2b=(int) (Math.random()*100);
+		
 	LineCompareOOPSUC1 val=new LineCompareOOPSUC1();
-	float length=val.cal_line(x1,y1,x2,y2);
-	System.out.println("The length is "+ length);
+	float length1=val.cal_line(x1,y1,x2,y2);
+	float lenght2=val.cal_line(x1b, y1b, x2b, y2b);
+	System.out.printf("The coordinates are (%.2f,%.2f) and (%.2f,%.2f)",x1,y1,x2,y2);
+	System.out.printf("\nThe coordinates are (%.2f,%.2f) and (%.2f,%.2f)",x1b,y1b,x2b,y2b);
+	val.comp_len(length1, lenght2);
 	}
 }
--- src/LineCompareOOPSUC1.java
+++ src/LineCompareOOPSUC1.java
@@ -4,5 +4,18 @@ class LineCompareOOPSUC1{
 		float len=(int)Math.sqrt(Math.pow((x1-x2),2)+(Math.pow((y2-y1), 2)));
 		return len;
 	}
-}
+	public static void comp_len(float len_a,float len_b)
+	{
+		Float length_1=new Float(len_a);
+		Float length_2=new Float(len_b);
+		if (length_1.equals(length_2)) {
+		System.out.println("Two lines are equal"+length_1.equals(length_2));
+		
+		}
+		else {
+		System.out.println("\nTwo lines are unequal");
+		
+		}
+	}
+}  
 
